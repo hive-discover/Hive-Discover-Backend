52ab527ea1abc92e66280c451e55002d9a8bbe91
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2020-11-06 18:29:07 +0100

    Adding

--- js_web/web_project/settings.py
+++ js_web/web_project/settings.py
@@ -16,12 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jubh-hcm1mf36p#v2x4r$+n5#s#u5nfs9(q)362u)s8j)2#o*f'
-
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
 #SECURE_SSL_REDIRECT = True
