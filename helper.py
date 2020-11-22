ec7e4f7404c67f87b1cb8f04d0ff504aaad407dd
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2020-11-22 13:47:16 +0100

    Adding some helpfing algorithm

--- helper.py
+++ helper.py
@@ -37,3 +37,32 @@ def pre_process_text(text):
     # return lower
     return text.lower()
 
+
+def calc_percentages(arr):
+    percs = []
+    total_amount = 0
+    for x in arr:
+        total_amount += x
+    
+    for x in arr: # % = Anteil/Gesamt
+        percs.append(float(x/total_amount))
+
+    return percs
+
+
+def get_top_elements(arr, amount):
+    top_ones = []   
+    arr = arr.copy()
+
+    while len(top_ones) < amount:
+        highest = (0, -1) # (value, arr_index)
+
+        for index, x in enumerate(arr):
+            if highest[1] == -1 or x > highest[0]:
+                # first or better
+                highest = (x, index)
+
+        top_ones.append(highest)
+        arr[highest[1]] = 0
+    
+    return top_ones
