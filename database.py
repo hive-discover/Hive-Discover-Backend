d83761ac2a3400dc5e9a8fa98b9309ed36b646b6
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2021-02-19 19:42:39 +0100

    Async MongoDB Client

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import motor.motor_asyncio
 from config import *
 
 
-class MongoDB:
+class MongoDBAsync:
     def __init__(self, post_table : bool = False, account_table : bool = False, banned_table : bool = False, stats_table : bool = False) -> None:
         self.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_HOST, DATABASE_PORT)
 
@@ -17,12 +17,36 @@ class MongoDB:
         if banned_table:
             self.banned_table = self.mongo_client[DATABASE_NAME].banned
             
+    @staticmethod
+    def init_global(post_table : bool = False, account_table : bool = False, banned_table : bool = False, stats_table : bool = False) -> None:
+        MongoDBAsync.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_HOST, DATABASE_PORT)
 
-    #   *** Static Methods ***
+        if post_table:
+            MongoDBAsync.post_table = MongoDBAsync.mongo_client[DATABASE_NAME].posts
+        if account_table:
+            MongoDBAsync.account_table = MongoDBAsync.mongo_client[DATABASE_NAME].accounts
+        if stats_table:
+            MongoDBAsync.stats_table = MongoDBAsync.mongo_client[DATABASE_NAME].stats
+        if banned_table:
+            MongoDBAsync.banned_table = MongoDBAsync.mongo_client[DATABASE_NAME].banned
+
+    
+class MongoDB:
+    def __init__(self, post_table : bool = False, account_table : bool = False, banned_table : bool = False, stats_table : bool = False) -> None:
+        self.mongo_client = pymongo.MongoClient(DATABASE_HOST, DATABASE_PORT)
+
+        if post_table:
+            self.post_table = self.mongo_client[DATABASE_NAME].posts
+        if account_table:
+            self.account_table = self.mongo_client[DATABASE_NAME].accounts
+        if stats_table:
+            self.stats_table = self.mongo_client[DATABASE_NAME].stats
+        if banned_table:
+            self.banned_table = self.mongo_client[DATABASE_NAME].banned
 
     @staticmethod
     def init_global(post_table : bool = False, account_table : bool = False, banned_table : bool = False, stats_table : bool = False) -> None:
-        MongoDB.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_HOST, DATABASE_PORT)
+        MongoDB.mongo_client = pymongo.MongoClient(DATABASE_HOST, DATABASE_PORT)
 
         if post_table:
             MongoDB.post_table = MongoDB.mongo_client[DATABASE_NAME].posts
@@ -32,5 +56,3 @@ class MongoDB:
             MongoDB.stats_table = MongoDB.mongo_client[DATABASE_NAME].stats
         if banned_table:
             MongoDB.banned_table = MongoDB.mongo_client[DATABASE_NAME].banned
-
-    
